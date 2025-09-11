65f685846b061957b2412aa2175bdeccca6b3e7c
CrewArth <arthvala@gmail.com>
2025-09-11 14:27:44 +0530

    uploaded JS task

--- javascript1.js
+++ javascript1.js
@@ -1,14 +1,3 @@
-// function varchar(){
-//     var a = 10;
-//     var b;
-
-//     console.log("A Value: ", a);
-//     console.log("B Value: ", b);
-
-//     let a = 15;
-//     let a;
-// }
-
 function calcBmi(){
     let markHeight = 1.69;
     let markWeight = 78;
@@ -33,3 +22,30 @@ function calcBmi(){
 // var → function-scoped, can be re-declared & updated.
 // let → block-scoped, can be updated but not re-declared in same scope.
 // const → block-scoped, cannot be re-assigned (value fixed, but objects/arrays can still mutate).
+
+
+let markData = {
+    'height': 1.69,
+    'weight': 78
+}
+
+let johnData = {
+    'height': 1.78,
+    'weight': 92
+}
+
+function calculateBmi(height, weight){
+    let bmi = weight / (height * height);
+
+    return bmi;
+}
+
+mark = calculateBmi(markData.height, markData.weight)
+john = calculateBmi(johnData.height, johnData.weight)
+
+if (mark > john){
+    console.log("Mark BMI is higher");
+}
+else{
+    console.log("John BMI is higher.");
+}
